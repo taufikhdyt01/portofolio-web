5ca898afabf81484db58133766c33130406efbf8
Raisjaya <ahmad.rdj2@gmail.com>
2022-08-24 21:53:13 +0700

    update resume

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -16,7 +16,7 @@ const Header = () => {
       <div className="container header__container">
         <h5 data-aos="zoom-in-up" data-aos-duration="1000" >Hello I'm</h5>
         <h1 data-aos="zoom-in-up" data-aos-duration="1200">Ahmad Rais Dwijaya</h1>
-        <h5 data-aos="fade-down" data-aos-duration="4500" className="text-light">Frontend Developer | UI UX Designer | Data Scientist</h5>
+        <h5 data-aos="fade-down" data-aos-duration="4500" className="text-light" >Frontend Developer | UI UX Designer | Data Scientist</h5>
         <CTA />
         <HeaderSocials/>
 
